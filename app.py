e96037d74e9f8ae4106f5308c2be7ce43e21118c
Nitin Kumar <nitinkumar6912@gmail.com>
2020-09-18 16:16:05 +0530

    Update app.py
    add: swagger-ui

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, send_from_directory
+from flask_swagger_ui import get_swaggerui_blueprint
 from flask_cors import CORS
 import logging
 from logging.handlers import RotatingFileHandler
@@ -25,6 +26,26 @@ app.config.from_object(app_settings)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 dashboard.bind(app)
+
+SWAGGER_URL = '/api/docs'
+API_URL = '/static/swagger.yml'
+
+swaggerui_blueprint = get_swaggerui_blueprint(
+    SWAGGER_URL,  # Swagger UI static files will be mapped to '{SWAGGER_URL}/dist/'
+    API_URL,
+    config={  # Swagger UI config overrides
+        'app_name': "N4NITIN_service"
+    },
+    # oauth_config={  # OAuth config. See https://github.com/swagger-api/swagger-ui#oauth2-configuration .
+    #    'clientId': "your-client-id",
+    #    'clientSecret': "your-client-secret-if-required",
+    #    'realm': "your-realms",
+    #    'appName': "your-app-name",
+    #    'scopeSeparator': " ",
+    #    'additionalQueryStringParams': {'test': "hello"}
+    # }
+)
+app.register_blueprint(swaggerui_blueprint, url_prefix=SWAGGER_URL)
 
 logging.basicConfig(level=logging.DEBUG)
 app.url_map.strict_slashes = False

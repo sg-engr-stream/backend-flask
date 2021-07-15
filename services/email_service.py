041c48feab992e767595e5b3c6d82bba91f5f46c
Nitin Kumar <nitinkumar6912@gmail.com>
2021-07-15 18:10:31 +0530

    Update email_service.py

--- services/email_service.py
+++ services/email_service.py
@@ -2,22 +2,26 @@ import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from email.utils import formatdate
+import os
 
 
 def send_mail(recipient, subject, body):
-    FROM = 'nitinkumaradm@gmail.com'
+    FROM = os.environ.get('Email_User')
     TO = recipient if type(recipient) is list else [recipient]
     SUBJECT = subject
     TEXT = body
-    secret = 'KillerBee@123'
+    secret = os.environ.get('Email_Pass')
     message = MIMEMultipart(From=FROM, Date=formatdate(localtime=True), Subject=SUBJECT)
     message['subject'] = SUBJECT
     message['to'] = ','.join(TO)
     message.attach(MIMEText(TEXT))
 
-    server = smtplib.SMTP("smtp.gmail.com", 587)
-    server.ehlo()
-    server.starttls()
-    server.login(FROM, secret)
-    server.sendmail(FROM, TO, message.as_string())
-    server.close()
+    server = smtplib.SMTP(os.environ.get('Email_SMTP'), os.environ.get('Email_Port'))
+    try:
+        server.ehlo()
+        server.starttls()
+        server.login(FROM, secret)
+        server.sendmail(FROM, TO, message.as_string())
+        server.close()
+    except Exception as e:
+        print(f"Email Exception: \n{str(e)}")

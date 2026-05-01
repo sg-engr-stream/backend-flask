ec94f74add67655762eafd8bf5b623d8eaa5eec6
Nitin Kumar <nitinkumar6912@gmail.com>
2026-05-01 17:25:01 +0530

    Implement resend

--- services/email_service.py
+++ services/email_service.py
@@ -1,7 +1,4 @@
-import smtplib
-from email.mime.multipart import MIMEMultipart
-from email.mime.text import MIMEText
-from email.utils import formatdate
+import resend
 import os
 
 
@@ -10,21 +7,14 @@ def send_mail(recipient, subject, body):
     TO = recipient if type(recipient) is list else [recipient]
     SUBJECT = subject
     TEXT = body
-    secret = os.environ.get('Email_Pass')
-    message = MIMEMultipart(From=FROM, Date=formatdate(localtime=True), Subject=SUBJECT)
-    message['subject'] = SUBJECT
-    message['to'] = ','.join(TO)
-    message.attach(MIMEText(TEXT))
+    resend.api_key = os.environ.get('RESEND_API_KEY')
 
-    smtp_server = os.environ.get('Email_SMTP') or ''
-    smtp_port = int(os.environ.get('Email_Port'))
-    
     try:
-        server = smtplib.SMTP_SSL(smtp_server, smtp_port, timeout=10)
-        server.login(FROM, secret)
-        server.sendmail(FROM, TO, message.as_string())
-        server.quit()
-    except smtplib.SMTPAuthenticationError as e:
-        print(f"SMTP Authentication Error: {str(e)}")
+        resend.Emails.send({
+            'from': FROM,
+            'to': TO,
+            'subject': SUBJECT,
+            'text': TEXT
+        })
     except Exception as e:
         print(f"Email Exception: \n{str(e)}")
